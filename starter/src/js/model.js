8c693e0f154d4050bf69655cc785ba1e7a5ca392
Specthildur <164398996+Specthildur@users.noreply.github.com>
2024-07-18 20:54:04 +0100

    t

--- starter/src/js/model.js
+++ starter/src/js/model.js
@@ -8,6 +8,7 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
 };
@@ -37,6 +38,7 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -51,7 +53,9 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-export const getSearchResultsPage = function (page) {
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
   const start = (page - 1) * state.search.resultsPerPage; //0;
   const end = page * state.search.resultsPerPage; // 9
 
